1aaa5d285175ba74bdd232f8213abc29198708b6
Josef Scheuer <webseppl@web.de>
2015-03-13 16:38:21 +0000

    finished initial version for writing makefile dependencies. TODO: finish preprocessor call via os.popen

--- rework/fort_deps.py
+++ rework/fort_deps.py
@@ -1,62 +1,75 @@
 
-"""Usage: fort_deps [--output=<outfile>] <file>...
+"""Usage: fort_deps [((-o|-a) <outfile>)] [--fpp=<cmd>] <file>...
 
 Generate Fortran dependencies.
 The result is output to stdout or given output file.
 
 Options:
-	--output=<outfile>		the output file
-
+	-o            overwrite given output file
+	-a            append to given output file
+	--fpp=<cmd>   preprocess each file by given command <cmd>.
+	              For complex commands use {} as a placeholder for the input file.
 """
 
-import docopt, re
-from extensions import Dict
+import docopt, re, sys, os
+from os.path import splitext
 
 class DepsCrawler(object):
 
-	_mod = re.compile( r'^\s*module\s+(\w+)\s*(?:!.*|)$', re.IGNORECASE ).match
-	_use = re.compile( r'^\s*use\s+(\w+)',                re.IGNORECASE ).match
+	_scanMod = re.compile( r'^\s*module\s+(\w+)\s*(?:!.*|)$', re.IGNORECASE ).match
+	_scanUse = re.compile( r'^\s*use\s+(\w+)',                re.IGNORECASE ).match
 
 	@staticmethod
-	def _cmp( a, b ):
-		if not a[1][1].isdisjoint( b[1][0] ): return 1
-		if not b[1][1].isdisjoint( a[1][0] ): return -1
-		return 0
+	def _obj( fileName ):
+		return splitext( fileName )[0] + '.o'
 
 
-	def __init__( self, **kwArgs ):
-		self._fileTab = Dict()
-		for fileName in kwArgs['<file>']:
-			self.scanFile( fileName )
-		
+	def __init__( self, fpp, fileList ):
+		self._fileTab, self._modTab = dict(), dict()
+		for fileName in fileList:
+			try  : self.scanFile( fileName, fpp )
+			except IOError as e:
+				print "%s skipped: %s" % (fileName, e)
+
+
+	def scanFile( self, fileName, fpp ):
+		with open( fileName ) as f:
+			uses = self._fileTab.setdefault( fileName, set() )
+			for mod in self.scanStream( f, uses ):
+				self._modTab[mod] = fileName
+
+
+	def scanStream( self, stream, uses ):
+		mods = set()
+		for line in stream.readlines():
+			mod = self._scanMod( line )
+			if mod:
+				mods.add( mod.groups()[0] ); continue
+			use = self._scanUse( line )
+			if use:
+				uses.add( use.groups()[0] ); continue
+		return mods
+
 
 	def __iter__( self ):
-		fileList = self._fileTab.items()
-		fileList.sort( self._cmp )
-		for f in fileList:
-			yield f
+		for f, uses in self._fileTab.items():
+			modFiles = set( map( self._modTab.get, uses ) ) - set([f])
+			yield self._obj(f) + ': ' + ' '.join( map( self._obj, filter( None, modFiles ) ) )
 
 
 	def __str__( self ):
-		return '\n'.join( map( str, self ) )
-
-
-	def scanFile( self, fileName ):
-			fileInfo = self._fileTab.setdefault( fileName, [] )
-			if not fileInfo:
-				fileInfo.extend( (set(),set()) ) #< ([<modSet>], [<useSet>])
-				with open( fileName ) as f:
-					for line in f.readlines():
-						mod = self._mod( line )
-						if mod: fileInfo[0].add( mod.groups()[0] )
-						else:
-							use = self._use( line )
-							if use: fileInfo[1].add( use.groups()[0] )
-
-
+		return '\n'.join( self )
 
 
 
 if __name__ == "__main__":
-	print DepsCrawler( **docopt.docopt( __doc__ ) )
+	opts = docopt.docopt( __doc__ )
+	print opts
+
+	outfile = opts.get('<outfile>')
+	if outfile: out = open( outfile, ('w', 'a')[opts.get('-a')] )
+	else      : out = sys.stdout
+
+	deps = DepsCrawler( opts.get('--fpp'), opts['<file>'] )
+	out.write( str(deps) + '\n' )
 
