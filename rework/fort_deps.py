68f6d25911bdcaa8abbb8605ffda7feae1e01c95
Josef Scheuer <webseppl@web.de>
2015-03-16 23:45:34 +0000

    added options for preprocessing fortran files and replacing directories

--- rework/fort_deps.py
+++ rework/fort_deps.py
@@ -1,18 +1,20 @@
 
-"""Usage: fort_deps [((-o|-a) <outfile>)] [--fpp=<cmd>] <file>...
+"""Usage: fort_deps [((-o|-a) <outfile>)] [--fpp=<cmd>] [--objdir=<dir>] <file>...
 
 Generate Fortran dependencies.
 The result is output to stdout or given output file.
 
 Options:
-	-o            overwrite given output file
-	-a            append to given output file
-	--fpp=<cmd>   preprocess each file by given command <cmd>.
-	              For complex commands use {} as a placeholder for the input file.
+	-o              overwrite given output file
+	-a              append to given output file
+	--fpp=<cmd>     preprocess each file by given command string <cmd>.
+	                Within <cmd> the placeholder {f} is replaced by the name of the file to preprocess.
+	--objdir=<dir>  replace directory of input files by given string <dir>.
 """
 
-import docopt, re, sys, os
-from os.path import splitext
+import docopt, re, sys, shlex
+from os.path    import splitext as _splitext, split as _splitpath, join as _joinpath
+from subprocess import Popen as _Popen, PIPE as _PIPE
 
 class DepsCrawler(object):
 
@@ -20,35 +22,39 @@ class DepsCrawler(object):
 	_scanUse = re.compile( r'^\s*use\s+(\w+)',                re.IGNORECASE ).match
 
 	@staticmethod
-	def _obj( fileName ):
-		return splitext( fileName )[0] + '.o'
+	def _getStream( fileName, fpp ):
+		if fpp: return _Popen( shlex.split( fpp.format( f=fileName ) ), stdout=_PIPE ).stdout
+		else  : return open( fileName, 'r' )
 
 
-	def __init__( self, fpp, fileList ):
+	def __init__( self, **kwArgs ):
+		self._objDir = kwArgs['--objdir']
+		if   self._objDir is None: self._setObjDir = lambda f: f
+		elif self._objDir        : self._setObjDir = lambda f: _joinpath( self._objDir, _splitpath(f)[-1] )
+		else                     : self._setObjDir = lambda f: _splitpath(f)[-1]
+
 		self._fileTab, self._modTab = dict(), dict()
-		for fileName in fileList:
-			try  : self.scanFile( fileName, fpp )
+		for fileName in kwArgs['<file>']:
+			try  : self.scanFile( fileName, kwArgs['--fpp'] )
 			except IOError as e:
 				print "%s skipped: %s" % (fileName, e)
 
 
 	def scanFile( self, fileName, fpp ):
-		with open( fileName ) as f:
+		with self._getStream( fileName, fpp ) as stream:
 			uses = self._fileTab.setdefault( fileName, set() )
-			for mod in self.scanStream( f, uses ):
-				self._modTab[mod] = fileName
+			for line in stream.readlines():
+				use = self._scanUse( line )
+				if use:
+					uses.add( use.groups()[0] )
+				else:
+					mod = self._scanMod( line )
+					if mod:
+						self._modTab[ mod.groups()[0] ] = fileName
 
 
-	def scanStream( self, stream, uses ):
-		mods = set()
-		for line in stream.readlines():
-			mod = self._scanMod( line )
-			if mod:
-				mods.add( mod.groups()[0] ); continue
-			use = self._scanUse( line )
-			if use:
-				uses.add( use.groups()[0] ); continue
-		return mods
+	def _obj( self, fileName ):
+		return self._setObjDir( _splitext( fileName )[0] + '.o' )
 
 
 	def __iter__( self ):
@@ -57,19 +63,13 @@ class DepsCrawler(object):
 			yield self._obj(f) + ': ' + ' '.join( map( self._obj, filter( None, modFiles ) ) )
 
 
-	def __str__( self ):
-		return '\n'.join( self )
-
-
 
 if __name__ == "__main__":
-	opts = docopt.docopt( __doc__ )
-	print opts
-
-	outfile = opts.get('<outfile>')
-	if outfile: out = open( outfile, ('w', 'a')[opts.get('-a')] )
+	options = docopt.docopt( __doc__ )
+	outfile = options.get('<outfile>')
+	if outfile: out = open( outfile, ('w', 'a')[options.get('-a')] )
 	else      : out = sys.stdout
 
-	deps = DepsCrawler( opts.get('--fpp'), opts['<file>'] )
-	out.write( str(deps) + '\n' )
+	deps = DepsCrawler( **options )
+	out.write( '\n'.join( deps ) + '\n' )
 
