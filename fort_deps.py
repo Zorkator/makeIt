5b0ab92396f533edbd8cb6a74c4373d31b8e6ecf
Josef Scheuer <webseppl@web.de>
2017-02-22 09:41:44 +0000

    fort_deps : reworked dependency finder to scan with multiple threads mk.fortran: enabled fpp file preprocessor for scanning dependencies

--- fort_deps.py
+++ fort_deps.py
@@ -1,107 +1,116 @@
 #!/usr/bin/env python
-"""Usage: fort_deps [((-a|-o) <outfile>)] [--verbose] [--fpp=<cmd>] [--objdir=<dir>] [--filelist=<listfile>] [<file>...]
+"""Usage:
+  fort_deps [-v...] [options] [FILE...]
+  fort_deps (-h | --help)
 
-Generate Fortran module dependencies.
-All given files are scanned for definitions and uses of modules.
-For module definitions fort_deps detects any legal fortran module start line _and_ preprocessor includes, matching the form:
-#include "[<path>]<module_name>.fmod"
-The result is written in make syntax to stdout or any given output file.
+Generate Fortran module dependencies. All given files are scanned for
+definitions and uses of modules. For module definitions fort_deps detects any
+legal fortran module start line _and_ preprocessor includes, matching the form:
+    #include "[<path>]<module_name>.fmod" The result is written in make syntax
+    to stdout or any given output file.
 
 Options:
-	-a                      append to given output file
-	-o                      overwrite given output file
-	--filelist=<listfile>   read list of (whitespace separated) input files from textfile <listfile>. Use '-' for reading from stdin.
-	--fpp=<cmd>             preprocess each file by given command string <cmd>.
-	                        Within <cmd> the placeholder {f} is replaced by the name of the file to preprocess.
-	--objdir=<dir>          replace directory of input files by given string <dir>.
-	--verbose               be verbose, print comment lines with process info.
+  -h --help                     show this help screen.
+  -i LISTFILE, --in LISTFILE    read list of (whitespace separated) input files from
+                                textfile LISTFILE. Use '-' for reading from stdin.
+  -o OUTFILE, --out OUTFILE     write output to file instead of dumping it to stdout.
+  -a --append                   append to given output file
+  -p CMD, --fpp CMD             preprocess each file by given command string CMD.
+                                Within CMD the placeholder {f} is replaced by the name of
+                                the file to preprocess.
+  -d DIR, --objdir DIR          replace directory of input files by given string DIR.
+  -j JOBS, --jobs JOBS          number of threads for processing files [default: 1].
+  -v                            set verbosity of printed messages. Increase level by
+                                repeating the option.
 """
 
-import docopt, re, sys, shlex, codecs
-from os.path    import splitext as _splitext, split as _splitpath, join as _joinpath
-from subprocess import Popen as _Popen, PIPE as _PIPE
+from os.path              import splitext as _splitext, split as _splitpath, join as _joinpath
+from subprocess           import Popen as _Popen, PIPE as _PIPE
+from multiprocessing.pool import ThreadPool
+import docopt, re, sys, logging
 
+logging.basicConfig()
 
 class DepsCrawler(object):
 
-	_scanMod   = re.compile( r'^\s*module\s+(\w+)\s*(?:!.*|)$',          re.IGNORECASE ).match
-	_scanInc   = re.compile( r'^#\s*include\s*["\'].*?(\w+)\.fmod["\']', re.IGNORECASE ).match
-	_scanUse   = re.compile( r'^\s*use\s+(\w+)',                         re.IGNORECASE ).match
-	_tryCodecs = ['latin-1', 'utf-8']
+  _scanMod   = re.compile( r'^\s*module\s+(\w+)\s*(?:!.*|)$',          re.IGNORECASE ).match
+  _scanInc   = re.compile( r'^#\s*include\s*["\'].*?(\w+)\.fmod["\']', re.IGNORECASE ).match
+  _scanUse   = re.compile( r'^\s*use\s+(\w+)',                         re.IGNORECASE ).match
+  _tryCodecs = ['latin-1', 'utf-8']
 
-	@staticmethod
-	def _getStream( fileName, fpp, codec ):
-		if fpp: return _Popen( shlex.split( fpp.format( f=fileName ) ), stdout=_PIPE ).stdout
-		else  : return codecs.open( fileName, 'r', codec )
-
-
-	def __init__( self, **kwArgs ):
-		if kwArgs['--verbose']: self._log = lambda *msg: self._out( 1, *msg )
-		else                  : self._log = lambda *msg: None
-
-		self._objDir = kwArgs['--objdir']
-		if   self._objDir is None: self._setObjDir = lambda f: f
-		elif self._objDir        : self._setObjDir = lambda f: _joinpath( self._objDir, _splitpath(f)[-1] )
-		else                     : self._setObjDir = lambda f: _splitpath(f)[-1]
-
-		fileList, fileSet = kwArgs['--filelist'], set( kwArgs['<file>'] )
-		if fileList:
-			with (fileList == '-' and sys.stdin) or open( fileList ) as listFile:
-				fileSet |= set( ' '.join( listFile.readlines() ).split() )
-
-		self._log( "processed files:", *fileSet )
-		self._fileTab, self._modTab = dict(), dict()
-		for fileName in fileSet:
-			try  : self.scanFile( fileName, kwArgs['--fpp'] )
-			except IOError as e:
-				self._out( 2, "WARNING: {0} skipped: {1}".format( fileName, e ) )
-		self._log( "\n", "module definitions:", *map( ': '.join, self._modTab.items() ) )
+  @staticmethod
+  def _getStream( fileName, fpp, codec ):
+    import shlex, codecs
+    if fpp: return _Popen( shlex.split( fpp.format( f=fileName ) ), stdout=_PIPE ).stdout
+    else  : return codecs.open( fileName, 'r', codec )
 
 
-	def _readFile( self, fileName, fpp ):
-		for codec in self._tryCodecs:
-			try:
-				with self._getStream( fileName, fpp, codec ) as stream:
-					return stream.readlines()
-			except UnicodeError:
-				self._out( 2, "WARNING: failed at decoding {0} by codec {1}".format( fileName, codec ) )
-		else:
-			raise UnicodeDecodeError( "unable to decode {0}".format( fileName ) )
+  def __init__( self, **kwArgs ):
+    self._log = logging.getLogger( self.__class__.__name__ )
+    self._log.setLevel( logging.WARNING - kwArgs['-v'] * 10 )
+    self._log.debug( kwArgs )
+
+    self._objDir = kwArgs['--objdir']
+    if   self._objDir is None: self._setObjDir = lambda f: f
+    elif self._objDir        : self._setObjDir = lambda f: _joinpath( self._objDir, _splitpath(f)[-1] )
+    else                     : self._setObjDir = lambda f: _splitpath(f)[-1]
+
+    fileList, fileSet = kwArgs['--in'], set( kwArgs['FILE'] )
+    if fileList:
+      with (fileList == '-' and sys.stdin) or open( fileList ) as listFile:
+        fileSet |= set( ' '.join( listFile.readlines() ).split() )
+
+    self._fpp = kwArgs['--fpp']
+    self._fileTab, self._modTab = dict(), dict()
+    ThreadPool( processes=int(kwArgs['--jobs']) ).map( self.scanFile, fileSet )
+    self._log.info( ' Module definitions:\n\t' +
+                    '\n\t'.join( map( ': '.join, self._modTab.items() ) ) )
 
 
-	def scanFile( self, fileName, fpp ):
-		uses = self._fileTab.setdefault( fileName, set() )
-		for line in self._readFile( fileName, fpp ):
-			use = self._scanUse( line )
-			if use:
-				uses.add( use.groups()[0].lower() )
-			else:
-				mod = self._scanMod( line ) or self._scanInc( line )
-				if mod:
-					self._modTab[ mod.groups()[0].lower() ] = fileName
+  def scanFile( self, fileName ):
+    try:
+      self._log.info( "scanning " + fileName )
+      uses = self._fileTab.setdefault( fileName, set() )
+      for line in self._readFile( fileName ):
+        use = self._scanUse( line )
+        if use:
+          uses.add( use.groups()[0].lower() )
+        else:
+          mod = self._scanMod( line ) or self._scanInc( line )
+          if mod:
+            self._modTab[ mod.groups()[0].lower() ] = fileName
+    except IOError as e:
+      self._log.error( "{0} skipped: {1}".format( fileName, e ) )
 
 
-	def _out( self, ch, *msg ):
-		(None, sys.stdout, sys.stderr)[ch].write( '# {0}\n'.format( '\n# '.join( msg ) ) )
+  def _readFile( self, fileName ):
+    for codec in self._tryCodecs:
+      try:
+        with self._getStream( fileName, self._fpp, codec ) as stream:
+          return stream.readlines()
+      except UnicodeError:
+        self._log.warning( "failed at decoding {0} by codec {1}".format( fileName, codec ) )
+    else:
+      raise UnicodeDecodeError( "unable to decode {0}".format( fileName ) )
 
 
-	def _obj( self, fileName ):
-		return self._setObjDir( _splitext( fileName )[0] + '.o' )
+  def _obj( self, fileName ):
+    return self._setObjDir( _splitext( fileName )[0] + '.o' )
 
 
-	def __iter__( self ):
-		for f, uses in self._fileTab.items():
-			modFiles = set( map( self._modTab.get, uses ) ) - set([f])
-			yield self._obj(f) + ': ' + ' '.join( map( self._obj, filter( None, modFiles ) ) )
+  def __iter__( self ):
+    for f, uses in self._fileTab.items():
+      modFiles = set( map( self._modTab.get, uses ) ) - set([f])
+      yield self._obj(f) + ': ' + ' '.join( map( self._obj, filter( None, modFiles ) ) )
 
 
 
 if __name__ == "__main__":
-	options = docopt.docopt( __doc__ )
-	outfile = options.get('<outfile>')
-	if outfile: out = open( outfile, ('w', 'a')[options.get('-a')] )
-	else      : out = sys.stdout
+  opts = docopt.docopt( __doc__ )
 
-	deps = DepsCrawler( **options )
-	out.write( '\n'.join( deps ) + '\n' )
+  try             : out = open( opts['--out'], 'wa'[opts['--append']] )
+  except TypeError: out = sys.stdout
+
+  deps = DepsCrawler( **opts )
+  out.write( '\n'.join( deps ) + '\n' )
 
